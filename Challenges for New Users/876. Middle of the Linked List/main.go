a7bd0a8d6e95b35c2715eaf5659be9d885742edb
nakama-polaris <nakama.polaris@gmail.com>
2023-05-06 08:28:35 +0900

    solved problem 876

--- Challenges for New Users/876. Middle of the Linked List/main.go
+++ Challenges for New Users/876. Middle of the Linked List/main.go
@@ -1,1 +1,23 @@
 package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+}
+
+func middleNode(head *ListNode) *ListNode {
+	listNodeArray := make([]*ListNode, 0)
+
+	// append head to listNodeArray while head is not nil
+	for head != nil {
+		listNodeArray = append(listNodeArray, head)
+		head = head.Next
+	}
+
+	// return the middle node of listNodeArray
+	return listNodeArray[len(listNodeArray)/2]
+}
